806bcca15ab93529e24644877914b16ba6becd1a
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2025-11-12 22:34:42 +0800

    feat: improve mobile responsiveness, display data from station context

--- src/components/navigation/SideBar.tsx
+++ src/components/navigation/SideBar.tsx
@@ -1,16 +1,20 @@
+//TODOS: Set logo to selected station and set query param to selected station
 "use client";
 import { open_sans } from "@/app/fonts";
 import { cn } from "@/lib/utils";
-import { MenuIcon } from "lucide-react";
+import { AtSignIcon, MenuIcon } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
-import { FaHome, FaPlay } from "react-icons/fa";
-import { FaMusic } from "react-icons/fa6";
+import { FaFacebook, FaHome, FaInstagram, FaMapMarkerAlt, FaPlay, FaTiktok } from "react-icons/fa";
+import { FaMusic, FaXTwitter, FaYoutube } from "react-icons/fa6";
 import { MdRadio } from "react-icons/md";
 import { useEffect, useState } from "react";
+import { useSelectedStationContext } from "@/context/StationWrapper";
+import { IoCall } from "react-icons/io5";
+import { LinkWithStationQuery } from "../LinkWithStationQuery";
 
 export function Sidebar({ }): React.JSX.Element {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const { selectedStation } = useSelectedStationContext();
 
     useEffect(() => {
         if (isMenuOpen) {
@@ -19,8 +23,9 @@ export function Sidebar({ }): React.JSX.Element {
             document.body.style.overflow = 'auto';
         }
     }, [isMenuOpen])
+
     return (
-        <div className={cn("sticky top-0 left-0 h-full w-full md:max-w-sidebar-width bg-greyspace z-40",
+        <div className={cn("sticky top-0 left-0 h-full w-full md:max-w-sidebar-width bg-greyspace z-40 md:z-20", // Adjust zIndex for mobile menu to avoid overlap with radio player
             open_sans.className
         )}>
             <div className="h-logo-container-height md:h-logo-container-height-md w-full text-white flex">
@@ -31,9 +36,10 @@ export function Sidebar({ }): React.JSX.Element {
                         className="absolute h-full w-full object-cover object-top-left"
                         quality={100}
                         fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                         priority
                     ></Image>
-                    <Link href="/"
+                    <LinkWithStationQuery href="/"
                         onClick={() => setIsMenuOpen(false)}>
                         <Image
                             src="/logo.png"
@@ -42,14 +48,13 @@ export function Sidebar({ }): React.JSX.Element {
                             width={100}
                             height={100}
                         />
-                    </Link>
+                    </LinkWithStationQuery>
                     <div className="relative md:hidden">
                         <MenuIcon size={24} className="text-white"
                             onClick={() => setIsMenuOpen(!isMenuOpen)}
                         />
                     </div>
                 </div>
-
             </div>
             <div className={cn(
                 "overflow-y-auto",
@@ -58,94 +63,143 @@ export function Sidebar({ }): React.JSX.Element {
             )}> {/* Adjust height to account for header and radio player */}
                 <div className="my-5 px-10">
                     <ul>
-                        <Link href="/" onClick={() => setIsMenuOpen(false)}>
+                        <LinkWithStationQuery href="/" onClick={() => setIsMenuOpen(false)}>
                             <li className="flex gap-2 items-center py-3 uppercase font-semibold text-grey-header">
                                 <FaHome size={24} />
                                 <span>Home</span>
                             </li>
-                        </Link>
-                        <Link href="/station" onClick={() => setIsMenuOpen(false)}>
+                        </LinkWithStationQuery>
+                        <LinkWithStationQuery href={{
+                            pathname: "/station",
+                            query: !selectedStation?.default && selectedStation?.slug ? { station: selectedStation.slug } : undefined
+                        }}
+                            onClick={() => setIsMenuOpen(false)}
+                        >
                             <li className="flex gap-2 items-center py-3 uppercase font-semibold text-grey-header">
                                 <MdRadio size={24} />
                                 <span>Listen Live</span>
                             </li>
-                        </Link>
-                        <Link href="/watch" onClick={() => setIsMenuOpen(false)}>
+                        </LinkWithStationQuery>
+                        <LinkWithStationQuery href="/watch" onClick={() => setIsMenuOpen(false)}>
                             <li className="flex gap-2 items-center py-3 uppercase font-semibold text-grey-header">
                                 <FaPlay size={22} />
                                 <span>Watch Live</span>
                             </li>
-                        </Link>
-                        <Link href="/jocks" onClick={() => setIsMenuOpen(false)}>
+                        </LinkWithStationQuery>
+                        <LinkWithStationQuery href="/jocks" onClick={() => setIsMenuOpen(false)}>
                             <li className="flex gap-2 items-center py-3 uppercase font-semibold text-grey-header">
                                 <FaMusic size={22} />
                                 <span>Programs</span>
                             </li>
-                        </Link>
+                        </LinkWithStationQuery>
                     </ul>
                 </div>
                 <hr className="border-t border-gray-300 mx-10" />
-                <div className="my-5 px-10">
-                    <h2 className="mt-10 mb-3 font-bold uppercase">Connect With Us</h2>
-                    {/* TODOS: change to icon library */}
-                    <ul className="flex gap-3 justify-center">
-                        <li>
-                            <a href="#"
-                                target="_blank"
-                                onClick={() => setIsMenuOpen(false)}
-                                className="flex items-center justify-center w-10 h-10 rounded-full bg-radyonatin-blue text-white hover:bg-opacity-80 transition-colors">
-                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
-                                </svg>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                target="_blank"
-                                onClick={() => setIsMenuOpen(false)}
-                                className="flex items-center justify-center w-10 h-10 rounded-full bg-radyonatin-blue text-white hover:bg-opacity-80 transition-colors">
-                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-                                </svg>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                target="_blank"
-                                onClick={() => setIsMenuOpen(false)}
-                                className="flex items-center justify-center w-10 h-10 rounded-full bg-radyonatin-blue text-white hover:bg-opacity-80 transition-colors">
-                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-                                </svg>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                target="_blank"
-                                onClick={() => setIsMenuOpen(false)}
-                                className="flex items-center justify-center w-10 h-10 rounded-full bg-radyonatin-blue text-white hover:bg-opacity-80 transition-colors">
-                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z" />
-                                </svg>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                target="_blank"
-                                onClick={() => setIsMenuOpen(false)}
-                                className="flex items-center justify-center w-10 h-10 rounded-full bg-radyonatin-blue text-white hover:bg-opacity-80 transition-colors">
-                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
-                                </svg>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                {selectedStation && !selectedStation.default && (
+                    <>
+                        <div className="my-5 px-5 grid gap-5">
+                            <div>
+                                <p className="text-lg font-bold">{selectedStation.name}</p>
+                            </div>
+                            {selectedStation.address && (
+                                <div className="flex items-center gap-3">
+                                    <div className="w-5 h-5 flex items-center justify-center">
+                                        <FaMapMarkerAlt size={20} />
+                                    </div>
+                                    <p>
+                                        {selectedStation.address}
+                                    </p>
+                                </div>
+                            )}
+                            {selectedStation.email && (
+                                <div className="flex items-center gap-3">
+                                    <div className="w-5 h-5 flex items-center justify-center">
+                                        <AtSignIcon size={20} />
+                                    </div>
+                                    <p>
+                                        {selectedStation.email}
+                                    </p>
+                                </div>
+                            )}
+                            {selectedStation.contactNumber && (
+                                <div className="flex items-center gap-3">
+                                    <div className="w-5 h-5 flex items-center justify-center">
+                                        <IoCall size={20} />
+                                    </div>
+                                    <p>
+                                        {selectedStation.contactNumber}
+                                    </p>
+                                </div>
+                            )}
+                        </div>
+                        <hr className="border-t border-gray-300 mx-10" />
+                    </>
+
+                )}
+                {selectedStation && !selectedStation.default && selectedStation.mapEmbedCode && (
+                    <div
+                        className="h-[300px] w-full"
+                        dangerouslySetInnerHTML={{ __html: selectedStation?.mapEmbedCode as string }} />
+                )}
+                {selectedStation?.socialLinks && Object.values(selectedStation.socialLinks).some(LinkWithStationQuery => LinkWithStationQuery && LinkWithStationQuery !== "") && (
+                    <div className="my-5 px-10">
+                        <h2 className="mb-3 font-bold uppercase">Connect With Us</h2>
+                        <ul className="flex justify-center gap-5">
+                            {selectedStation?.socialLinks?.facebook &&
+                                <li>
+                                    <a href={selectedStation.socialLinks.facebook}
+                                        target="_blank"
+                                        onClick={() => setIsMenuOpen(false)}
+                                        className="flex items-center justify-center w-10 h-10 rounded-full bg-radyonatin-blue text-white hover:bg-opacity-80 transition-colors">
+                                        <FaFacebook className="w-5 h-5" />
+                                    </a>
+                                </li>
+                            }
+                            {selectedStation?.socialLinks?.twitter &&
+                                <li>
+                                    <a href={selectedStation.socialLinks.twitter}
+                                        target="_blank"
+                                        onClick={() => setIsMenuOpen(false)}
+                                        className="flex items-center justify-center w-10 h-10 rounded-full bg-radyonatin-blue text-white hover:bg-opacity-80 transition-colors">
+                                        <FaXTwitter className="w-5 h-5" />
+                                    </a>
+                                </li>
+                            }
+                            {selectedStation?.socialLinks?.instagram &&
+                                <li>
+                                    <a href={selectedStation.socialLinks.instagram}
+                                        target="_blank"
+                                        onClick={() => setIsMenuOpen(false)}
+                                        className="flex items-center justify-center w-10 h-10 rounded-full bg-radyonatin-blue text-white hover:bg-opacity-80 transition-colors">
+                                        <FaInstagram className="w-5 h-5" />
+                                    </a>
+                                </li>
+                            }
+                            {selectedStation?.socialLinks?.tiktok &&
+                                <li>
+                                    <a href={selectedStation.socialLinks.tiktok}
+                                        target="_blank"
+                                        onClick={() => setIsMenuOpen(false)}
+                                        className="flex items-center justify-center w-10 h-10 rounded-full bg-radyonatin-blue text-white hover:bg-opacity-80 transition-colors">
+                                        <FaTiktok className="w-5 h-5" />
+                                    </a>
+                                </li>
+                            }
+                            {selectedStation?.socialLinks?.youtube &&
+                                <li>
+                                    <a href={selectedStation.socialLinks.youtube}
+                                        target="_blank"
+                                        onClick={() => setIsMenuOpen(false)}
+                                        className="flex items-center justify-center w-10 h-10 rounded-full bg-radyonatin-blue text-white hover:bg-opacity-80 transition-colors">
+                                        <FaYoutube className="w-5 h-5" />
+                                    </a>
+                                </li>
+                            }
+                        </ul>
+                    </div>
+                )}
                 <div className="my-5 flex items-center justify-center">
                     <div className="relative h-[250px] w-[300px] bg-gray-300 my-5">
-                        <Image src={`https://picsum.photos/600/400?random=${Math.floor(Math.random() * 10) + 1}`} alt="Sample"
-                            fill
-                            className="absolute inset-0 object-cover" />
                     </div>
                 </div>
                 <hr className="border-t border-gray-300 mx-10" />
