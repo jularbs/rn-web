a459de4f9557163da2cc31555964b64a85df6e6f
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2025-11-19 15:29:23 +0800

    chore: Override path url sharing

--- src/components/RadioPlayer.tsx
+++ src/components/RadioPlayer.tsx
@@ -66,7 +66,7 @@ export function RadioPlayer({ }): React.JSX.Element {
                     </div>
                 </div>
                 <div className="hidden lg:flex bg-radyonatin-blue h-12 w-px" />
-                <SocialSharingButton className="" >
+                <SocialSharingButton className="" path="/">
                     <div className="flex items-center">
                         <div className="text-radyonatin-blue px-2 lg:px-5 py-2">
                             <Share size={20} />
