4e0ce6589050291ebf6f5885c8515815a730df00
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2025-11-26 16:15:00 +0800

    #RNW-13: Implement Video Player and use stream data from station default

--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -3,18 +3,14 @@ import FacebookShareComponent from "@/components/FacebookShareComponent";
 import { LatestStoriesComponent } from "@/components/LatestStoriesComponent";
 import SocialSharingButton from "@/components/SocialSharingButton";
 import TwitterShareComponent from "@/components/TwitterShareComponent";
-import Image from "next/image";
+import VideoLiveStreaming from "@/components/VideoLiveStreaming";
 
 export default function watch() {
 
     return (
         <div className="flex flex-col xl:flex-row gap-5 p-5">
             <div className="flex-1">
-                <h1 className="text-3xl font-extrabold mb-5 text-grey-header">WATCH LIVE!</h1>
-                <div className="aspect-video w-full bg-gray-200 flex justify-center items-center">
-                </div>
-                {/* TODOS: Set font to Karla */}
-                <p className="text-2xl font-extrabold my-3">Radio Natin Nationwide</p>
+                <VideoLiveStreaming />
                 <div className="flex gap-2 mt-5 items-center font-semibold">
                     <span className="text-xs">Share</span>
                     <FacebookShareComponent />
@@ -27,20 +23,13 @@ export default function watch() {
                 <div className="sticky top-5">
                     <div className="flex justify-center items-center mb-3">
                         <div className="relative h-[250px] w-[300px] bg-gray-300 my-5">
-                            <Image src={`https://picsum.photos/600/400?random=${Math.floor(Math.random() * 10) + 1}`} alt="Sample"
-                                fill
-                                className="absolute inset-0 object-cover" />
                         </div>
                     </div>
                     <div className="flex justify-center items-center mb-3">
                         <div className="relative h-[250px] w-[300px] bg-gray-300 my-5">
-                            <Image src={`https://picsum.photos/600/400?random=${Math.floor(Math.random() * 10) + 1}`} alt="Sample"
-                                fill
-                                className="absolute inset-0 object-cover" />
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     );
