3e91c94b4846170d1f1ebe764035a528007004ab
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2025-12-03 18:47:57 +0800

    hotfix: remove placeholder imagse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { LatestStoriesComponent } from "@/components/LatestStoriesComponent";
 import { PostsLandingPageComponent } from "@/components/PostsLandingPageComponent";
 import { StationDetailsComponent } from "@/components/StationDetailsComponent";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,16 +11,11 @@ export default function Home() {
         <LatestStoriesComponent />
         <div className="flex justify-center items-center mb-3">
           <div className="relative h-[250px] w-[300px] bg-gray-300 my-5">
-            <Image src={`https://picsum.photos/600/400?random=${Math.floor(Math.random() * 10) + 1}`} alt="Sample"
-              fill
-              className="absolute inset-0 object-cover" />
+
           </div>
         </div>
         <div className="flex justify-center items-center mb-3">
           <div className="relative h-[250px] w-[300px] bg-gray-300 my-5">
-            <Image src={`https://picsum.photos/600/400?random=${Math.floor(Math.random() * 10) + 1}`} alt="Sample"
-              fill
-              className="absolute inset-0 object-cover" />
           </div>
         </div>
       </div>
