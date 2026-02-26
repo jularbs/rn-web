f020d3cfce13d772f9b41bb0202f8fe8b17ebeb4
Chooks22 <49726759+Chooks22@users.noreply.github.com>
2026-02-26 23:48:05 +0800

    fix: config bucket url

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,10 @@ const nextConfig: NextConfig = {
   images: {
     qualities: [75, 80, 90, 100],
     remotePatterns: [
+      new URL(process.env.NEXT_PUBLIC_BUCKET_URL ?? 'https://radyonatin-bucket.s3.ap-southeast-1.amazonaws.com'),
       {
-        protocol: "https",
-        hostname: "**",
+        protocol: 'https',
+        hostname: '**',
       },
     ],
   },
