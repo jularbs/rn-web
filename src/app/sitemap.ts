f4e73b7c34f224842207e574061e551742cee1bb
Chooks22 <49726759+Chooks22@users.noreply.github.com>
2026-03-10 18:55:55 +0800

    fix: update env var name

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from "next";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
 const BASE_URL = process.env.NEXT_PUBLIC_DOMAIN || "https://radyonatin.com";
 
 interface Post {
